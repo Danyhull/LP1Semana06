44f640804ac389c582c45ed16bbeb18da1eee2b1
Daniel Silva <danielhull.silva1@gmail.com>
2024-04-11 22:53:18 +0000

    Classe player refeita

--- PlayerStats/Player.cs
+++ PlayerStats/Player.cs
@@ -2,41 +2,55 @@ using System;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Drawing;
 
-
 namespace PlayerStats
 {
-    public class Player
-    {
+    class Player{
+
         private float highScore;
         private int playedGames;
-        private int wonGames
-
-        public float highScore
-        {
-            get { return highScore; }
-        }
-        set
-        {
-            if (highScore < value)
+        private int wonGames;
+   
+        public float HighScore{
+            get 
+            { 
+                return highScore;
+            }
+            set
             {
-                highScore = value;
+                if (highScore < value){
+                    highScore = value;
+                }           
             }
         }
-    }
+        public string Name{get;}
 
-    public void PlayGame(bool win)
-    {
-        playedGames += 1;
-        if (win)
-        {
-            wonGames += 1;
+        public float WinRate{
+            get
+            {
+                if (playedGames == 0){
+                    return 0;
+                }        
+                else{
+                    return (float)wonGames/ playedGames;
+                }
+                
+            }
         }
+
+        public void PlayGame(bool win){
+            playedGames += 1;
+            if (win){
+                wonGames += 1;
+            }
+        }
+        public Player(string name){
+            Name = name;
+            highScore = 0;
+            playedGames = 0;
+            wonGames = 0;
+
+        }   
     }
-    public Player (string name)
-    {
-        Name = name;
-        highScore = 0;
-        playedGames = 0;
-        wonGames = 0;
-    }
+    
+    
 }
