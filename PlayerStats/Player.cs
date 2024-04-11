b78e96eaedd87a36c5a0b69e67ef08b9721e75fe
Daniel Silva <danielhull.silva1@gmail.com>
2024-04-11 22:51:17 +0000

    Mian e player atualizados

--- PlayerStats/Player.cs
+++ PlayerStats/Player.cs
@@ -1,12 +1,42 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Drawing;
+
 
 namespace PlayerStats
 {
     public class Player
     {
-        
+        private float highScore;
+        private int playedGames;
+        private int wonGames
+
+        public float highScore
+        {
+            get { return highScore; }
+        }
+        set
+        {
+            if (highScore < value)
+            {
+                highScore = value;
+            }
+        }
+    }
+
+    public void PlayGame(bool win)
+    {
+        playedGames += 1;
+        if (win)
+        {
+            wonGames += 1;
+        }
+    }
+    public Player (string name)
+    {
+        Name = name;
+        highScore = 0;
+        playedGames = 0;
+        wonGames = 0;
     }
 }
--- PlayerStats/Program.cs
+++ PlayerStats/Program.cs
@@ -1,12 +1,56 @@
-﻿using System;
+﻿﻿using System;
 
 namespace PlayerStats
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Player[] players = {
+                new Player("Ana"),
+                new Player("Zé"),
+                new Player("Doesn't play")
+            };
+
+            players[0].PlayGame(false);
+            players[0].PlayGame(true);
+            players[0].PlayGame(true);
+            players[0].PlayGame(false);
+            players[0].PlayGame(false);
+            players[0].PlayGame(true);
+            players[0].HighScore = 101;
+            players[0].HighScore = 40;
+
+            players[1].PlayGame(true);
+            players[1].PlayGame(true);
+            players[1].HighScore = 12;
+            players[1].HighScore = 67;
+            players[1].HighScore = 99;
+            players[1].HighScore = 32;
+
+            foreach (Player player in players)
+            {
+                Console.WriteLine($" Player name : {player.Name}");
+                Console.WriteLine($"    Win rate : {player.WinRate}");
+                Console.WriteLine($"  High score : {player.HighScore}");
+                Console.WriteLine("--------------------------------");
+            }
+
+            // Output deverá de ser:
+            //
+            //  Player name : Lurdes
+            //     Win rate : 0.5
+            //   High score : 101
+            // --------------------------------
+            //  Player name : Carlos
+            //     Win rate : 1
+            //   High score : 99
+            // --------------------------------
+            //  Player name : Doesn't play
+            //     Win rate : 0
+            //   High score : 0
+            // --------------------------------
+
         }
     }
 }
